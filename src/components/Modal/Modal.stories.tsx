23e4d13af35403b2f2d139e3e6e7baa7f6ccac8b
Dulaj Kavinda <dulajkaavinda@gmail.com>
2023-08-02 15:50:17 +0530

    feat: stories for modal component added

---
+++ src/components/Modal/Modal.stories.tsx
@@ -0,0 +1,100 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import Modal from './Modal'
+import { Group } from './Modal.types'
+
+const meta = {
+  title: 'Modal',
+  component: Modal,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof Modal>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+const listA: Group[] = [
+  {
+    sectionName: 'Repositories',
+    items: [
+      {
+        icon: '🗂',
+        title: 'How to make a good repo',
+        url: '/repo',
+      },
+      {
+        icon: '🚀',
+        title: 'This is another one to check',
+        url: 'https://google.lk',
+      },
+    ],
+  },
+  // {
+  //   sectionName: 'Projects',
+  //   items: [
+  //     {
+  //       icon: '🏋️‍♀️',
+  //       title: 'How to make a good repo',
+  //     },
+  //     {
+  //       icon: '📸',
+  //       title: 'This is another one to check',
+  //     },
+  //   ],
+  // },
+  // {
+  //   sectionName: 'Something',
+  //   items: [
+  //     {
+  //       icon: '🏋️‍♀️',
+  //       title: 'How to make a good repo',
+  //       url: 'https://google.lk',
+  //     },
+  //     {
+  //       icon: '📸',
+  //       title: 'This is another one to check',
+  //       url: 'https://google.lk',
+  //     },
+  //     {
+  //       icon: '🏋️‍♀️',
+  //       title: 'How to make a good repo',
+  //       url: 'https://google.lk',
+  //     },
+  //     {
+  //       icon: '📸',
+  //       title: 'This is another one to check',
+  //       url: 'https://google.lk',
+  //     },
+  //   ],
+  // },
+]
+
+export const Small: Story = {
+  args: {
+    data: listA,
+    isOpen: true,
+    toggle: () => {},
+    size: 'small',
+  },
+}
+
+export const Medium: Story = {
+  args: {
+    data: listA,
+    isOpen: true,
+    toggle: () => {},
+    size: 'medium',
+  },
+}
+
+export const Large: Story = {
+  args: {
+    data: listA,
+    isOpen: true,
+    toggle: () => {},
+    size: 'large',
+  },
+}
